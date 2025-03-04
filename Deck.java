72d2742fa4ab1829d0d23a078f16fda004a91c35
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-04 20:47:27 +0530

    constructor defination

--- Deck.java
+++ Deck.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 public class Deck {
-    
+    private ArrayList<Cards> deck;
+    public Deck(){
+        createDeck();
+    }
+    public void createDeck(){
+        
+    }
 }
