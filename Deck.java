c39a0c549c3ee58b6ed27bbb236291841c8a14d5
Jojohere05 <2005jhotikaraja@gmail.com>
2025-03-04 20:50:20 +0530

    create deck

--- Deck.java
+++ Deck.java
@@ -7,6 +7,13 @@ public class Deck {
         createDeck();
     }
     public void createDeck(){
-        
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks={"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        deck = new ArrayList<>();
+        for (String suit:suits){
+            for (String rank:ranks){
+                deck.add(new Cards(suit, rank));
+            }
+        }
     }
 }
